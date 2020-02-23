4d82b0c15a17ae4d0319038186787fccb7c58b1f
Yurii Antonchyk <yurii.antonchyk@gmail.com>
2020-02-23 19:03:25 +0200

    Added not found response

--- public/index.php
+++ public/index.php
@@ -1,2 +1,10 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+use Worker\Application;
+
+(new Application())->run();
--- src/Application.php
+++ src/Application.php
@@ -1,3 +1,39 @@
 <?php
 
 declare(strict_types=1);
+
+namespace Worker;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class Application
+ */
+class Application
+{
+    /**
+     * @var Request
+     */
+    private Request $request;
+
+    /**
+     * @var Response
+     */
+    private Response $response;
+
+    /**
+     * Application constructor.
+     */
+    public function __construct()
+    {
+        $this->request = Request::createFromGlobals();
+        $this->response = new JsonResponse(['message' => 'Not Found'], 404);
+    }
+
+    public function run(): void
+    {
+        $this->response->send();
+    }
+}
